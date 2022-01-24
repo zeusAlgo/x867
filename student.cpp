be5bbc515affc50bdd0f87c8cf4b01a10ef32dc2
zeusalgo <email@example.com>
2022-01-24 10:48:16 -0600

    m

--- student.cpp
+++ student.cpp
@@ -23,10 +23,18 @@ string Student::getAge() {return age;}
 int Student::getDays(int atArray) {return daysInCourse[atArray];}
 DegreeProgram Student::getDegreeProgram() {return degreeProgram;}
 
-void Student::setId(string studentId) {this -> studentId = move(studentId);}
-void Student::setFirstName(string firstName) {this -> firstName = move(firstName);}
-void Student::setLastName(string lastName) {this -> lastName = move(lastName);}
-void Student::setEmailAddress(string emailAddress) {this -> emailAddress = move(emailAddress);}
+void Student::setId(string studentId) {
+    this -> studentId = move(studentId);
+}
+void Student::setFirstName(string firstName) {
+    this -> firstName = move(firstName);
+}
+void Student::setLastName(string lastName) {
+    this -> lastName = move(lastName);
+}
+void Student::setEmailAddress(string emailAddress) {
+    this -> emailAddress = move(emailAddress);
+}
 void Student::setAge(string age) {this -> age = std::move(age);}
 void Student::setDays(int int0, int int1, int int2) {
     this -> daysInCourse[0] = int0;
