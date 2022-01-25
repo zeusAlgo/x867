a95be3650359153edc83a83871e14d0ebe48e69a
zeusalgo <email@example.com>
2022-01-25 08:31:19 -0600

    m

--- roster.cpp
+++ roster.cpp
@@ -137,14 +137,14 @@ void Roster::parseArray(int rosterIndex) {
 
 void Roster::printAll() const {
     for (int idx = 0; idx < 5; idx++) {
-//        Student currentStudent = classRosterArray[idx];
         auto * currentStudent = new Student;
         currentStudent = classRosterArray[idx];
-        
+
         if (classRosterArray[idx] -> getId() != "NULL" &&
                 classRosterArray[idx] != classRosterArray[idx -1]) {
             //TODO: assign cra[idx] to var for continual reference lower computation
             //currStudent = cra[idx]
+            cout << currentStudent->getId(); printDivider();
             cout << classRosterArray[idx]->getId(); printDivider();
             cout << classRosterArray[idx]->getFirstName(); printSpace();
             cout << classRosterArray[idx]->getLastName(); printDivider();
