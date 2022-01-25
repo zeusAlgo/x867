ba414a28f2129f213b355072e9a837877f68c5cc
zeusalgo <email@example.com>
2022-01-25 09:40:30 -0600

    m

--- roster.h
+++ roster.h
@@ -7,7 +7,7 @@ using namespace std;
 
 const string studentData[5] = {
         "A1,John,Smith,John1989@gm ail.com,20,30,35,40,SECURITY",
-        "A2,Suzan,Erickson,Erickson_199@gmail.com,19,50,30,40,NETWORK",
+        "A2,Suzan,Erickson,Erickson_199@gmailc,19,50,30,40,NETWORK",
         "A3,Jack,Napoli,The_lawyer99@yahoo.com,19,20,40,33,SOFTWARE",
         "A4,Erin,Black,Erin.black@comcast.net,22,40,58,40,SECURITY",
         "A5,Regis,Tribbet,registribbet@gmail.com,10,20,200,SOFTWARE"
