694167b5152434f412fe2e7122c3f3947473ab48
zeusalgo <email@example.com>
2022-01-25 15:58:08 -0600

    m

--- roster.cpp
+++ roster.cpp
@@ -190,10 +190,9 @@ void Roster::printByDegreeProgram(DegreeProgram degreeProgram) const {
     for (int idx = 0; idx < 5; idx++) {
         Student *currentStudent;
         currentStudent = classRosterArray[idx];
-        cout << currentStudent->getId();
         if (degreeProgram == currentStudent->getDegreeProgram()) {
             //TODO: investigate why this for loop isn't executing
-                cout << currentStudent->getId();
+            cout << currentStudent->getId();
                 cout << " ";
                 cout << "First Name: ";
                 cout << currentStudent -> getFirstName();
