721fc38509540fe076077cf1217a6f97279aac66
zeusalgo <email@example.com>
2022-01-25 09:49:01 -0600

    m

--- roster.h
+++ roster.h
@@ -2,7 +2,6 @@
 #include "student.h"
 #include <unordered_map>
 #include <string>
-
 using namespace std;
 
 const string studentData[5] = {
@@ -13,11 +12,6 @@ const string studentData[5] = {
         "A5,Regis,Tribbet,registribbet@gmail.com,10,20,200,SOFTWARE"
 };
 
-//string usr[9];
-//std::unordered_map<int, string[9]> usrMap;
-//usrMap[0] =
-
-//usrMap.insert(make_pair(0, {"A1","John","Smith","John1989@gmail.com","20","30","35","40","SECURITY"};));
 class Roster : public error_code {
 public:
     Roster();
