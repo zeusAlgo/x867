214c39a07f7453127a60ee73f21f451c8b31a44e
zeusalgo <email@example.com>
2022-01-25 11:12:57 -0600

    m

--- roster.cpp
+++ roster.cpp
@@ -186,7 +186,7 @@ void Roster::printByDegreeProgram(DegreeProgram degreeProgram) const {
         default:
             cout << "NULL"; break;
     }
-    cout << "" << endl;
+    cout << "hey" << endl;
     for (int idx = 0; idx < 5; idx++) {
         auto * currentStudent = classRosterArray[idx];
         if (currentStudent -> getDegreeProgram() == degreeProgram) {
