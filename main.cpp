106eafbee8ba0468a0460c52f27c6ac69066e118
zeusalgo <email@example.com>
2022-01-25 09:52:28 -0600

    m

--- main.cpp
+++ main.cpp
@@ -14,6 +14,7 @@ int main() {
     classRoster -> printAll();
     classRoster -> printInvalidEmails();
 
+//    cout << "Printing mean days in course for each student: " << endl;
     Roster::showHeader(
             "Printing mean days in course for each student: ");
     for (int idx = 0; idx < 5; idx++) {
