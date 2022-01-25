3cd0c06b6ae4cbea3bb9a3a702ef31e9be139240
zeusalgo <email@example.com>
2022-01-25 09:57:21 -0600

    m

--- roster.cpp
+++ roster.cpp
@@ -147,7 +147,7 @@ void Roster::printAll() const {
             printDegreeString(currentStudent->getDegreeProgram());
             cout << "\n-------------------------" << endl;
         }
-    }
+    } cout << "\n" << endl;
 }
 
 void Roster::printDivider() {
