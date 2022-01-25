f1993eb57c3d125a82637a20775f0648ad0c18c3
zeusalgo <email@example.com>
2022-01-24 18:23:18 -0600

    m

--- roster.cpp
+++ roster.cpp
@@ -153,7 +153,7 @@ void Roster::printAll() {
             cout << classRosterArray[idx]->getDays(1);cout << ", ";
             cout << classRosterArray[idx]->getDays(2) ;cout << " \n";
             cout << "Degree Program: ";
-            cout << classRosterArray[idx] -> getDegreeProgram();
+            printDegreeString(classRosterArray[idx] -> getDegreeProgram());
 //            printDegreeString(classRosterArray[idx] -> getDegreeProgram());
             cout << "\n-------------------------" << endl;
         }
