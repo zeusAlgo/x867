34ed47e6c5fa4cf53d5b552f0424902c000da67a
zeusalgo <email@example.com>
2022-01-25 10:37:46 -0600

    m

--- roster.cpp
+++ roster.cpp
@@ -52,7 +52,8 @@ void Roster::removeStudent(const string& sId) const {
             cout << "Done";
             break;
         }
-    }  cout << "Student" << sId << "not found.";
+    }
+    cout << "Student" << sId << "not found.";
 }
 
 void Roster::parseArray(int rosterIndex) {
