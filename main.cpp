8e83d99de6d66742325ef05c7ebb26b900406295
zeusalgo <email@example.com>
2022-01-26 15:22:58 -0600

    m

--- main.cpp
+++ main.cpp
@@ -23,6 +23,10 @@ int main() {
 
     cout << endl;
     classRoster -> printByDegreeProgram(SOFTWARE);
+
+    classRoster -> printByDegreeProgram(SECURITY);
+
+    classRoster -> printByDegreeProgram(NETWORK);
     cout << endl;
     classRoster -> removeStudent("A3");
     cout << endl;
