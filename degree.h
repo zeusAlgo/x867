abd0a81022368b965e5d7a40c64042a33dfd80bd
zeusAlgo <36017479+zeusAlgo@users.noreply.github.com>
2022-11-06 07:39:17 -0600

    Update degree.h

--- degree.h
+++ degree.h
@@ -1,5 +1,4 @@
 #pragma once
 #include <string>
-// DEF MEANING? - CHANGED DEFAULT TO INIT
+
 enum DegreeProgram {SECURITY, NETWORK, SOFTWARE, INIT};
-// software corresponds to 3? why? requires investigation
